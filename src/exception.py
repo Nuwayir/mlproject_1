cbb04f25513410a5020feb8410799d5896b81582
Nuwayir <nuwaiyr@gmail.com>
2024-09-03 17:23:34 +0300

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+#sys : contain the details of any error if happend 
+# NOTE : make sure to look into sys "custom exception handling in python" documention
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()# return od sys i'm just intersted in the 
+                                    # 3th one so i'm skiping the first two _,_,
+    file_name=exc_tb.tb_frame.f_code.co_filename # in the exception handeling documnetion 
+    error_message="Erorr occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+       file_name,exc_tb.tb_lineno,str(error)
+    )
+       
+    return error_message 
+   
+# NOTE: WHENVER error raised i will call this function !!
+
+
+class CustomException(Exception):# inherit from the Exception 
+    # the constructer 
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)# beacuse we are inheriting form the Exception 
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    # to ensurewhen it  printed it print the error message 
---
+++ src/logger.py
@@ -0,0 +1,25 @@
+# to track any logging information even the exceptions see where is the errors 
+
+import logging
+import os 
+from datetime import datetime
+
+# create the log file with time format:s -> i cna take the format from the documention 
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# give a path for the LOG_FIEL: 
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)# getcwd : current working dic -> src 
+                                    # star witl log ... whatever name of the LOG_FILE
+os.makedirs(logs_path,exist_ok=True)#even though there is a file keep adding new files as well !                                  
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO # Adjust this to the desired logging level
+)
+
+# NOTE: to test : 
+if __name__=="__main__":
+    logging.info("Logging has started")
+    # now it will make folder insied it a file with the message as the format above 

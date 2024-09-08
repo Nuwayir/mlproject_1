7435a65989e84177e6915d92ded1c5653271b96e
Nuwayir <nuwaiyr@gmail.com>
2024-09-08 15:00:11 +0300

    EDA AND Problem statment

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,10 @@
 import sys
+from src.logger import logging
 #sys : contain the details of any error if happend 
-# NOTE : make sure to look into sys "custom exception handling in python" documention
+# NOTE : make sure to look into sys "custom exception handling in python" documentation
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()# return od sys i'm just intersted in the 
+    _,_,exc_tb=error_detail.exc_info()# return of sys i'm just intersted in the 
                                     # 3th one so i'm skiping the first two _,_,
     file_name=exc_tb.tb_frame.f_code.co_filename # in the exception handeling documnetion 
     error_message="Erorr occured in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -24,3 +25,13 @@ class CustomException(Exception):# inherit from the Exception
     def __str__(self):
         return self.error_message
     # to ensurewhen it  printed it print the error message 
+
+# for test:
+# if __name__=="__main__":
+    
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)     
+        
--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ import logging
 import os 
 from datetime import datetime
 
-# create the log file with time format:s -> i cna take the format from the documention 
+# create the log file with time format:s -> i can take the format from the documention 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 # give a path for the LOG_FIEL: 
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)# getcwd : current working dic -> src 
@@ -20,6 +20,6 @@ logging.basicConfig(
 )
 
 # NOTE: to test : 
-if __name__=="__main__":
-    logging.info("Logging has started")
-    # now it will make folder insied it a file with the message as the format above 
+# if __name__=="__main__":
+#     logging.info("Logging has started")
+#     # now it will make folder insied it a file with the message as the format above 
